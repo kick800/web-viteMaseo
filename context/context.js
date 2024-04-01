068d9cfc45c6433282cfd90bb6355632b954c9dc
imaginuz <zunig4business@gmail.com>
2024-04-01 17:23:16 -0600

    Create folders

---
+++ context/context.js
@@ -0,0 +1,18 @@
+import indexPageContext from './indexPage';
+import galeriaPageContext from './galleryPage';
+
+const pageContext = {
+    "/index.html": indexPageContext,
+    "/pages/gallery.html": galeriaPageContext
+}
+
+const globalContext = {
+    "title": "ONG - MASEO"
+}
+
+const getContext = (contextKey) => {
+    const _pageContext = pageContext[contextKey] || {};
+    return Object.assign({}, globalContext, _pageContext);
+}
+
+export default getContext;
