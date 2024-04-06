8a156a209a7f317586bc01ea42f89bdddbad3a04
Fabriicii0 <fabricioescobar623@gmail.com>
2024-04-05 23:28:54 -0600

    agg nosotros _ context

--- context/context.js
+++ context/context.js
@@ -1,9 +1,11 @@
 import indexPageContext from './indexPage';
 import galeriaPageContext from './galleryPage';
+import cardsPage from './cardsPage';
 
 const pageContext = {
     "/index.html": indexPageContext,
-    "/pages/gallery.html": galeriaPageContext
+    "/pages/gallery.html": galeriaPageContext,
+    "/pages/nosotros.html": cardsPage,
 }
 
 const globalContext = {
