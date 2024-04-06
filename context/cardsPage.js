e2a55e6d9873e8a3438dbd6b63cb1d63d8f682e0
Fabriicii0 <fabricioescobar623@gmail.com>
2024-04-05 23:27:52 -0600

    hbs cards

---
+++ context/cardsPage.js
@@ -0,0 +1,20 @@
+export default {
+    todos: [
+        {
+            "imgUrl": "../img/nuestra_ciencia.png",
+            "name": "Nuestra ciencia",
+        },
+        {
+            "imgUrl": "../img/nuestra_historia.png",
+            "name": "Nuestra Historia"
+        },
+        {
+            "imgUrl": "../img/nuestra_diversidad.png",
+            "name": "Nuestra diversidad"
+        },
+        {
+            "imgUrl": "../img/nuestros_valores.png",
+            "name": "Nuestros valores"
+        }
+    ]
+}
