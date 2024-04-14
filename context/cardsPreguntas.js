9e6edce20f8e270be26cd12e9a2e982c7ef17c37
Asshlly <144860915+Asshlly@users.noreply.github.com>
2024-04-14 12:50:10 -0600

    Avancebu
    Avance bueno

---
+++ context/cardsPreguntas.js
@@ -0,0 +1,29 @@
+export default {
+    "todo": [
+        {
+        pre: "¿Cómo soy voluntario en The Nature Conservancy (TNC)?",
+        content : "Encuentre una oportunidad de voluntariado visitando nuestra página de Oportunidades de voluntariado . Cada oportunidad de voluntariado individual incluye información sobre cómo participar, incluido si se requiere alguna preparación, como firmar formularios o viajar a un lugar específico. Las oportunidades de voluntariado se basan en las necesidades y la capacidad de conservación. Si el área en la que le gustaría ser voluntario no aparece en la lista, comuníquese directamente con la oficina de TNC para ver qué oportunidades podrían estar disponibles. Las oportunidades de voluntariado pueden o no estar disponibles con nuestros proyectos en países en el extranjero; Si está interesado en ser voluntario fuera de los Estados Unidos, comuníquese con nuestra oficina en esa región para obtener más información. ¿Preguntas adicionales? Póngase en contacto con Megan Whatton , directora del programa Nature Allies."
+        },
+        {
+            pretwo: "¿Qué tipos de oportunidades de voluntariado están disponibles?",
+            contenttwo : "Hay una amplia gama de oportunidades de voluntariado disponibles para individuos y grupos en una variedad de entornos, tanto en interiores como en exteriores. En las reservas de TNC y en las tierras de nuestros socios, los voluntarios desempeñan un papel clave en la restauración del hábitat, el monitoreo de especies, la dirección de recorridos, los programas educativos y culturales, y más. En ciudades seleccionadas, trabajamos con múltiples socios para apoyar la acción comunitaria que se presenta de muchas formas, como plantar árboles, restaurar espacios verdes y/o monitorear la biodiversidad, mitigar el calor urbano o proteger la calidad del agua. Los voluntarios también brindan apoyo en nuestras oficinas y aportan su experiencia en proyectos."
+        },
+        {
+            pretres: "¿Necesito experiencia para ser voluntario en TNC?",
+            contentres: "La mayoría de nuestros roles voluntarios no requieren experiencia previa. Valoramos las muchas contribuciones más allá del tiempo en especie que brindan los voluntarios, como experiencias vividas, conexiones, perspectivas comunitarias y pasión. Si se requiere capacitación, se proporcionará."
+        },
+        {
+            precua: "¿Existen oportunidades para que los grupos se ofrezcan como voluntarios en TNC?",
+            contentcua : "Este trabajo requiere muchas manos (y corazones) y damos la bienvenida a grupos cívicos, grupos de exploradores, grupos religiosos, grupos corporativos y otros para ayudar a restaurar, monitorear y cuidar la naturaleza. Por ejemplo, los grupos pueden eliminar plantas invasoras, recolectar y sembrar semillas nativas, plantar árboles y más. Comuníquese con su oficina local de TNC sobre oportunidades para organizar un grupo de voluntariado o una actividad de participación comunitaria."
+        },
+        {
+            precin: "¿Puedo traer a mi familia como voluntario?",
+            contentcin : "Damos la bienvenida a voluntarios de todas las edades y habilidades. La mayoría de nuestras oportunidades de voluntariado son para familias, pero siempre es bueno comprobarlo primero. Comuníquese con su oficina local de TNC para obtener más información sobre las actividades de voluntariado específicas cerca de usted."
+       
+        },
+        {
+            preseis: "¿Mi tiempo de voluntariado en TNC puede acreditarse para servicio escolar o comunitario?",
+            contentsies : " Devolverle a la naturaleza no sólo es divertido, sino también una excelente manera de ganar horas de servicio comunitario y una oportunidad de aprender más sobre el campo de la conservación. Nuestro personal local trabajará con usted para documentar el tiempo dedicado al voluntariado como créditos adicionales o servicio comunitario."
+        }
+    ],
+}
